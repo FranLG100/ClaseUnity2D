f0663ed69eff91f3f176cc7662dd92a0e60bbc1a
Fran <fralg100@gmail.com>
2019-10-22 20:09:08 +0200

    Primer commit

---
+++ UnityClase2D/Assets/Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    [Range(0.02f,0.2f)]
+    public float parallaxSpeed = 0.02f;
+    public RawImage background;
+    public RawImage platform;
+    public GameObject uiIdle;
+
+    public enum GameState { Idle,Playing };
+    public GameState gameState = GameState.Idle;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))) {
+            gameState = GameState.Playing;
+            uiIdle.SetActive(false);
+            player.SendMessage("UpdateState", "PlayerRun");
+        }
+        else if (gameState==GameState.Playing) {
+            Parallax();
+        }
+        
+    }
+
+    void Parallax() {
+        float finalSpeed = parallaxSpeed * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+    }
+}
---
+++ UnityClase2D/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        //animator.Play("PlayerRun");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)) {
+            UpdateState("PlayerJump");
+        }
+	}
+
+    public void UpdateState(string state) {
+        if (state != null) {
+            animator.Play(state);
+        }
+    }
+}
