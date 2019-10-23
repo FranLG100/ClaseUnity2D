9edccbe8895b9e9958a3a96be40ed50dc87a77db
Fran <fralg100@gmail.com>
2019-10-23 13:03:49 +0200

    Tut 15

--- UnityClase2D/Assets/Assets/Scripts/GameController.cs
+++ UnityClase2D/Assets/Assets/Scripts/GameController.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public enum GameState { Idle, Playing, Ended };
+public enum GameState { Idle, Playing, Ended, Ready };
 
 public class GameController : MonoBehaviour {
 
@@ -19,27 +20,35 @@ public class GameController : MonoBehaviour {
     public GameObject player;
     public GameObject enemyGenerator;
 
+    private AudioSource musicPlayer;
+
 	// Use this for initialization
 	void Start () {
-		
+        musicPlayer = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))) {
+        bool userAction = Input.GetKeyDown("up") || Input.GetMouseButtonDown(0);
+
+        if (gameState == GameState.Idle && userAction) {
             gameState = GameState.Playing;
             uiIdle.SetActive(false);
             player.SendMessage("UpdateState", "PlayerRun");
             enemyGenerator.SendMessage("StartGenerator");
+            musicPlayer.Play();
         }
         else if (gameState==GameState.Playing) {
             Parallax();
         }
 
-        else if (gameState == GameState.Ended)
+        else if (gameState == GameState.Ready)
         {
-            Parallax();
+            if (userAction)
+            {
+                RestartGame();
+            }
         }
 
     }
@@ -48,5 +57,9 @@ public class GameController : MonoBehaviour {
         float finalSpeed = parallaxSpeed * Time.deltaTime;
         background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
         platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+    }
+
+    public void RestartGame() {
+        SceneManager.LoadScene("Nivel");
     }
 }
--- UnityClase2D/Assets/Assets/Scripts/PlayerController.cs
+++ UnityClase2D/Assets/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,29 @@ public class PlayerController : MonoBehaviour {
 
     public GameObject game;
     public GameObject enemyGenerator;
+    public AudioClip jumpClip;
+    public AudioClip dieClip;
     private Animator animator;
+    private AudioSource audioPlayer;
+    private float startY;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
+        audioPlayer = GetComponent<AudioSource>();
         //animator.Play("PlayerRun");
+        startY = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        bool isGrounded = transform.position.y == startY;
         bool gamePlaying = game.GetComponent<GameController>().gameState == GameState.Playing;
-        if (gamePlaying && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))) {
+        bool userAction = Input.GetKeyDown("up") || Input.GetMouseButtonDown(0);
+        if (isGrounded && gamePlaying && userAction) {
             UpdateState("PlayerJump");
+            audioPlayer.clip = jumpClip;
+            audioPlayer.Play();
         }
 	}
 
@@ -34,7 +44,14 @@ public class PlayerController : MonoBehaviour {
         {
             UpdateState("PlayerDie");
             game.GetComponent<GameController>().gameState = GameState.Ended;
-            enemyGenerator.SendMessage("CancelGenerator",true);
+            enemyGenerator.SendMessage("CancelGenerator", true);
+            game.GetComponent<AudioSource>().Stop();
+            audioPlayer.clip = dieClip;
+            audioPlayer.Play();
         }
+    }
+
+    public void GameReady() {
+        game.GetComponent<GameController>().gameState = GameState.Ready;
     }
 }
